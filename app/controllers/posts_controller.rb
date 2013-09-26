858760d1a87b4a59a817dc4093cc82ae8eb34104
robzdc <robz.del.castillo@gmail.com>
2013-09-26 23:55:34 +0000

    Se agrego la referencia al post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.joins(:category).select("posts.id, posts.title, posts.link, posts.status, posts.post_type, posts.comment_status, categories.name")
+    @posts = Post.joins(:category).select("posts.id, posts.title, posts.link, posts.refer, posts.status, posts.post_type, posts.comment_status, categories.name").where("posts.status = ?",1)
   end
 
   # GET /posts/1
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Admin::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  
+  config.assets.compile = true
+  config.assets.digest = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
